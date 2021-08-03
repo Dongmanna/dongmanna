965279206035262b623045d1688186024623957e
summerlunaa <rhgus1085@gmail.com>
2021-08-04 00:56:49 +0900

    Implement accounts

--- dmnproject/urls.py
+++ dmnproject/urls.py
@@ -1,21 +1,8 @@
-"""dmnproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# dmnproject/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
+import accounts.views
 # for uploading media files
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +14,8 @@ urlpatterns = [
     path('detail/<int:pk>', views.detail, name="detail"),
     path('edit/<int:pk>', views.edit, name="edit"),
     path('detail/<int:pk>/delete', views.delete, name="delete"),
+
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
