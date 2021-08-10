2e326e43663b2609f8c7bc32b02be63b0c0d5e9a
gilgor2 <misby0327@gmail.com>
2021-08-10 14:02:25 +0900

    front2 for pull

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,13 +1,22 @@
 
-window.addEventListener('scroll',changeColor);
-function changeColor(){
+window.addEventListener('scroll',scrollEvent);
+// document.querySelector(".search-box").style.transform="translate(0,10px)";
+// document.querySelector(".search-box").style.opacity="1";
+
+function scrollEvent(){
     let scrHeight = document.documentElement.scrollTop;
-    if(scrHeight<800){
-        document.querySelector(".main-main").style.backgroundColor="seagreen";
+    console.log(scrHeight);
+    if(20<scrHeight && scrHeight<1200){
+        document.querySelector("body").style.backgroundColor="#9dc39c";
+    } else if( 1200 <=scrHeight && scrHeight<2150){
+        document.querySelector("body").style.backgroundColor="#fbfaf6";
     } else {
-        document.querySelector(".main-main").style.backgroundColor="#a7a856";
+        document.querySelector("body").style.backgroundColor="#9dc39c";
     }
-    if (scrHeight>=400 && scrHeight <1400){
+    
+        
+        // 박스2 애니메이션 
+    if (scrHeight>=1100 && scrHeight <2200){
         document.querySelector(".my-group").style.transform="translate(0,0)";
         document.querySelector(".my-group").style.opacity="1";
     } else{
