b9d8859dc8898ccfbf74aa5ada63be553204dac2
summerlunaa <rhgus1085@gmail.com>
2021-08-10 18:17:52 +0900

    fix main.views.home

--- main/urls.py
+++ main/urls.py
@@ -5,13 +5,11 @@ from . import views
 
 
 urlpatterns = [
-    path('main/', views.home, name="home"),
-    path('category/<str:category>/',views.home_category , name='home_category'),
+    path('home/<str:category>', views.home, name="home"),
     path('new/', views.new, name="new"),
     path('detail/<int:pk>', views.detail, name="detail"),
     path('edit/<int:pk>', views.edit, name="edit"),
     path('detail/<int:pk>/delete', views.delete, name="delete"),
     path('search/', views.SearchFormView.as_view(), name="search"),
     path('participated/<int:pk>/', views.post_participated_toggle, name="post_participated_toggle"),
-    
 ]
